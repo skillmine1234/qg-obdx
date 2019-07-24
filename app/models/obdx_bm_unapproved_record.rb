92bd402ac8eefe06718eb1e768314891aa2e45e3
Durgasree Boddu <durgasree.boddu@capgemini.com>
2019-07-24 12:26:34 +0530

    seq issue removed

--- app/models/obdx_bm_unapproved_record.rb
+++ app/models/obdx_bm_unapproved_record.rb
@@ -1,5 +1,6 @@
 class ObdxBmUnapprovedRecord < ActiveRecord::Base
-  establish_connection "obdx_#{Rails.env}"	
+  establish_connection "obdx_#{Rails.env}"
+  self.sequence_name = "bm_unapproved_records"	
   	
   belongs_to :obdx_bm_approvable, :polymorphic => true, :unscoped => true
   BM_TABLES = ['ObdxBmRule','ObdxBmBiller','ObdxBmAggregatorPayment','ObdxBmApp']
