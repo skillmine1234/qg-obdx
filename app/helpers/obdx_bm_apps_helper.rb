a585574ae757c0eac1a2fda375ba1d4cff90ef9a
skillmine1234 <rahul.khoragade@skill-mine.com>
2023-12-27 19:21:19 +0530

    new changes in query

--- app/helpers/obdx_bm_apps_helper.rb
+++ app/helpers/obdx_bm_apps_helper.rb
@@ -2,8 +2,8 @@ module ObdxBmAppsHelper
   
   def find_bm_apps(params)
     bm_apps = (params[:approval_status].present? and params[:approval_status] == 'U') ? ObdxBmApp.unscoped : ObdxBmApp
-    bm_apps = bm_apps.where("app_id=?",params[:app_id]) if params[:app_id].present?
-    bm_apps = bm_apps.where("channel_id=?",params[:channel_id]) if params[:channel_id].present?
+    bm_apps = bm_apps.where("LOWER(app_id) LIKE ?", "%#{params[:app_id].downcase}%") if params[:app_id].present?
+    bm_apps = bm_apps.where("LOWER(channel_id) LIKE ?", "%#{params[:channel_id].downcase}%") if params[:channel_id].present?
     bm_apps
   end
 end
--- app/helpers/obdx_bm_billers_helper.rb
+++ app/helpers/obdx_bm_billers_helper.rb
@@ -2,12 +2,12 @@ module ObdxBmBillersHelper
 
   def find_bm_billers(params)
     bm_billers = ObdxBmBiller.all
-    bm_billers = bm_billers.where("billerid=?",params[:billerid]) if params[:billerid].present?
-    bm_billers = bm_billers.where("biller_name=?",params[:biller_mode]) if params[:biller_mode].present?
-    bm_billers = bm_billers.where("biller_category=?",params[:biller_category]) if params[:biller_category].present?
-    bm_billers = bm_billers.where("biller_name=?",params[:biller_name]) if params[:biller_name].present?
-    bm_billers = bm_billers.where("biller_location=?",params[:biller_name]) if params[:biller_location].present?
-    bm_billers = bm_billers.where("biller_type=?",params[:biller_type]) if params[:biller_type].present?
+    bm_billers = bm_billers.where("LOWER(billerid) LIKE ?", "%#{params[:billerid].downcase}%") if params[:billerid].present?
+    bm_billers = bm_billers.where("LOWER(biller_name) LIKE ?", "%#{params[:biller_name].downcase}%") if params[:biller_name].present?
+    bm_billers = bm_billers.where("LOWER(biller_category) LIKE ?", "%#{params[:biller_category].downcase}%") if params[:biller_category].present?
+    bm_billers = bm_billers.where("LOWER(biller_name) LIKE ?", "%#{params[:biller_name].downcase}%") if params[:biller_name].present?
+    bm_billers = bm_billers.where("LOWER(biller_location) LIKE ?", "%#{params[:biller_location].downcase}%") if params[:biller_location].present?
+    bm_billers = bm_billers.where("LOWER(biller_type) LIKE ?", "%#{params[:biller_type].downcase}%") if params[:biller_type].present?
     bm_billers
   end
 
